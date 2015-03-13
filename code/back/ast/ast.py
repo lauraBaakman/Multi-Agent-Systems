37003975e5a84122c68136f8f7d745fd6d1e6fa6
Laura Baakman <laurabaakman@gmail.com>
2015-03-13 16:27:06 +0100

    Added dictionary with precedences.

--- code/back/ast/ast.py
+++ code/back/ast/ast.py
@@ -6,9 +6,26 @@ Class that defines the abstract syntax tree
 
 __author__ = 'laura'
 
+import tokenize.operators as operators
 
 class Ast(object):
     """Class to represent an abstract syntax tree."""
+
+    _precedence = {
+        operators.Unary.common : 1,
+        operators.Modal.knowledge : 1,
+        operators.Modal.possible : 1,
+
+        operators.Unary.negation : 2,
+
+        operators.Binary.conjunction : 3,
+
+        operators.Binary.disjunction : 4,
+
+        operators.Binary.implication : 5,
+
+        operators.Binary.biimplication : 6
+    }
 
     def __init__(self, formula):
         """Create an AST from formula.
