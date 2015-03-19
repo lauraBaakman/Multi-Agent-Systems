b2952211673d75b51501cbec8a059fd56be47f7c
Laura Baakman <laurabaakman@gmail.com>
2015-03-19 11:37:11 +0100

    .

--- code/back/main.py
+++ code/back/main.py
@@ -7,8 +7,8 @@ Main for coding
 import tokenize.tokenizer as tokenizer
 import parser
 from ast import ast
-
 import models.kmmodel
+import models.errors
 import utils
 
 
@@ -18,20 +18,27 @@ if __name__ == "__main__":
     logic = "KM"
     filename = '../model.json'
 
-    tree = {}
     try:
         tree = ast.Ast.from_string(formula, logic)
+        print tree
+
+        data = utils.read_json(filename)
+        model = models.kmmodel.KMModel.from_json(data)
+        print model
     except tokenizer.TokenizeError as e:
         print e.msg
     except parser.ParserError as e:
         print e.message
+    except models.errors.ModelError as e:
+        print e.message
+    except models.errors.ValuationError as e:
+        print e.message
 
-    print tree
 
-    data = utils.read_json(filename)
-    model = models.kmmodel.KMModel.from_json(data)
 
-    print model
+
+
+
 
 
 
