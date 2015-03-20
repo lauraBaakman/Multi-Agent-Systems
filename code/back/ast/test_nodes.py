abede91111dbc61119092e054060a01f27f3d43d
Laura Baakman <laurabaakman@gmail.com>
2015-03-20 14:22:16 +0100

    Unit tests draaien ook weer.

--- code/back/ast/test_nodes.py
+++ code/back/ast/test_nodes.py
@@ -1,31 +1,24 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
 
-from nodes import Binary, Proposition
+from nodes import Proposition, Binary
 from models.kmmodel import KMModel
-from tokenize import operators, tokens
+from tokenize import operators
 import utils
 
 __author__ = 'laura'
 
 class TestBinary(TestCase):
-    # TODO Check deze statements met model
 
     def setUp(self):
         self.model = KMModel()
         json_data = utils.read_json('./models/test_model_km.json')
         self.model = KMModel.from_json(json_data)
-        self.lhs = Proposition(tokens.Proposition('p'))
-        self.rhs = Proposition(tokens.Proposition('q'))
+        self.lhs = Proposition('p')
+        self.rhs = Proposition('q')
 
     def test_is_true_conjunction(self):
-        node = Binary(
-            tokens.BinaryOperator(
-                operators.Binary.conjunction
-            )
-        )
-        node.lhs = self.lhs
-        node.rhs = self.rhs
+        node = Binary(operators.Binary.conjunction, self.lhs, self.rhs)
 
         self.assertTrue(node.is_true(self.model.get_state_by_name('sa')))
         self.assertFalse(node.is_true(self.model.get_state_by_name('sb')))
@@ -33,13 +26,7 @@ class TestBinary(TestCase):
         self.assertFalse(node.is_true(self.model.get_state_by_name('sd')))
 
     def test_is_true_disjunction(self):
-        node = Binary(
-            tokens.BinaryOperator(
-                operators.Binary.disjunction
-            )
-        )
-        node.lhs = self.lhs
-        node.rhs = self.rhs
+        node = Binary(operators.Binary.disjunction, self.lhs, self.rhs)
 
         self.assertTrue(node.is_true(self.model.get_state_by_name('sa')))
         self.assertTrue(node.is_true(self.model.get_state_by_name('sb')))
@@ -47,13 +34,7 @@ class TestBinary(TestCase):
         self.assertTrue(node.is_true(self.model.get_state_by_name('sd')))
 
     def test_is_true_implication(self):
-        node = Binary(
-            tokens.BinaryOperator(
-                operators.Binary.implication
-            )
-        )
-        node.lhs = self.lhs
-        node.rhs = self.rhs
+        node = Binary(operators.Binary.implication, self.lhs, self.rhs)
 
         self.assertTrue(node.is_true(self.model.get_state_by_name('sa')))
         self.assertFalse(node.is_true(self.model.get_state_by_name('sb')))
@@ -61,13 +42,7 @@ class TestBinary(TestCase):
         self.assertTrue(node.is_true(self.model.get_state_by_name('sd')))
 
     def test_is_true_biimplication(self):
-        node = Binary(
-            tokens.BinaryOperator(
-                operators.Binary.biimplication
-            )
-        )
-        node.lhs = self.lhs
-        node.rhs = self.rhs
+        node = Binary(operators.Binary.biimplication, self.lhs, self.rhs)
 
         self.assertTrue(node.is_true(self.model.get_state_by_name('sa')))
         self.assertFalse(node.is_true(self.model.get_state_by_name('sb')))
@@ -83,17 +58,17 @@ class TestProposition(TestCase):
         pass
 
     def test_is_true(self):
-        node = Proposition(tokens.Proposition('p'))
+        node = Proposition('p')
         self.assertTrue(node.is_true(self.model.get_state_by_name('sa')))
         self.assertTrue(node.is_true(self.model.get_state_by_name('sb')))
         self.assertFalse(node.is_true(self.model.get_state_by_name('sc')))
 
-        node = Proposition(tokens.Proposition('q'))
+        node = Proposition('q')
         self.assertTrue(node.is_true(self.model.get_state_by_name('sa')))
         self.assertFalse(node.is_true(self.model.get_state_by_name('sb')))
         self.assertFalse(node.is_true(self.model.get_state_by_name('sc')))
 
-        node = Proposition(tokens.Proposition('r'))
+        node = Proposition('r')
         self.assertFalse(node.is_true(self.model.get_state_by_name('sa')))
         self.assertTrue(node.is_true(self.model.get_state_by_name('sb')))
         self.assertTrue(node.is_true(self.model.get_state_by_name('sc')))
