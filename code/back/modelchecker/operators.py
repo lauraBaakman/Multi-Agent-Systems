a9cbad39637fcac5ef1e8c862fd6bcc6addaa948
Laura Baakman <laurabaakman@gmail.com>
2015-03-26 19:31:01 +0100

    Added support ofr everybody.

--- code/back/modelchecker/operators.py
+++ code/back/modelchecker/operators.py
@@ -15,37 +15,13 @@ class Binary(Operator):
     implication = 3
     biimplication = 4
 
-    def to_latex(self):
-        operator_to_latex = {
-            Binary.conjunction : '\\land',
-            Binary.disjunction : '\\lor',
-            Binary.implication : '\\to',
-            Binary.biimplication : '\\leftrightarrow'
-        }
-        return operator_to_latex.get(self)
 
 class Unary(Operator):
     negation = 5
     common = 6
-    # TODO everybody operator implementeren!!!!
     everybody = 7
-
-    def to_latex(self):
-        operator_to_latex = {
-            Unary.negation: '\\lnot',
-            Unary.common: '\\text{C}\\:',
-            Unary.everybody: '\\text{E}\\:'
-        }
-        return operator_to_latex.get(self)
 
 
 class Agent(Operator):
     knowledge = 8
     possible = 9
-
-    def to_latex(self):
-        operator_to_latex = {
-            Agent.knowledge: '\\text{K}',
-            Agent.possible: '\\text{M}\\:'
-        }
-        return operator_to_latex.get(self)
