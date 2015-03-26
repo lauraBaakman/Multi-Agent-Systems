60093056716160f1da683a17078b41a273a2f5f3
Laura Baakman <laurabaakman@gmail.com>
2015-03-26 09:03:44 +0100

    Transitive closure.

--- code/back/modelchecker/utils/closures.py
+++ code/back/modelchecker/utils/closures.py
@@ -14,3 +14,20 @@ def reflexive(original, states):
     """
     reflexive_relations = [(state, state) for state in states]
     return original.union(reflexive_relations)
+
+def transitive(original):
+    """
+    Compute the transitive closure of the original set.
+    :param original: the set of which the transitive closure should be computed.
+    :type original: set
+    :return: the transitive closure of the original set
+    :rtype: set
+    """
+    closure = original
+    while True:
+        new_relations = set((x, w) for x, y in closure for q, w in closure if q == y)
+        closure_until_now = closure | new_relations
+        if closure_until_now == closure:
+            break
+        closure = closure_until_now
+    return closure
--- code/back/modelchecker/utils/test_closures.py
+++ code/back/modelchecker/utils/test_closures.py
@@ -11,6 +11,13 @@ class TestReflexive(TestCase):
     def test_reflexive(self):
         states = [1, 2, 3, 4]
         original_set = set([(1, 2), (2,3)])
-        expected_result = set([(1,1), (2,2), (3,3), (4,4), (1,2), (2,3)])
+        expected_result = original_set.union([(1,1), (2,2), (3,3), (4,4)])
         computed_result = reflexive(original_set, states)
+        self.assertEquals(expected_result, computed_result)
+
+class TestTransitive(TestCase):
+    def test_transitive(self):
+        original_set = set([(1, 2), (2, 3), (3, 4)])
+        expected_result = original_set.union([(1,3), (1,4), (2, 4)])
+        computed_result = transitive(original_set)
         self.assertEquals(expected_result, computed_result)
