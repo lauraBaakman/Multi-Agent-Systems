7d2e1368d7697ce901cb69717572397a16773e20
Laura Baakman <laurabaakman@gmail.com>
2015-03-27 09:32:00 +0100

    Added test for tokenization of propositonal logic.

--- code/back/modelchecker/tokenize/test_tokenize.py
+++ code/back/modelchecker/tokenize/test_tokenize.py
@@ -3,14 +3,75 @@ from unittest import TestCase
 from modelchecker.tokenize.tokenizer import tokenize
 import modelchecker.operators as operators
 from modelchecker.tokenize import tokens
+import modelchecker.config as config
 
 __author__ = 'laura'
 
 
-class TestTokenizeKM(TestCase):
+class TestTokenizeProposition(TestCase):
+    def setUp(self):
+        self.logics = config.logics
+
     def test_tokenize_proposition(self):
-        logic = 'KM'
         expression = 'p'
-        computed = tokenize(logic, expression)
-        expected = [tokens.Proposition('p')]
-        self.assertEqual(computed, expected)
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [tokens.Proposition('p')]
+            self.assertItemsEqual(computed, expected)
+
+    def test_tokenize_conjunction(self):
+        expression = 'p & q'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.Proposition('p'),
+                tokens.BinaryOperator(operators.Binary.conjunction),
+                tokens.Proposition('q')
+            ]
+            self.assertEqual(computed, expected)
+
+    def test_tokenize_disjunction(self):
+        expression = 'p | q'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.Proposition('p'),
+                tokens.BinaryOperator(operators.Binary.disjunction),
+                tokens.Proposition('q')
+            ]
+            self.assertEqual(computed, expected)
+
+    def test_tokenize_implication(self):
+        expression = 'p -> q'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.Proposition('p'),
+                tokens.BinaryOperator(operators.Binary.implication),
+                tokens.Proposition('q')
+            ]
+            self.assertEqual(computed, expected)
+
+    def test_tokenize_biimplication(self):
+        expression = 'p <-> q'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.Proposition('p'),
+                tokens.BinaryOperator(operators.Binary.biimplication),
+                tokens.Proposition('q')
+            ]
+            self.assertEqual(computed, expected)
+
+    def test_tokenize_brackets(self):
+        expression = '(p & q)'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.BracketOpen(),
+                tokens.Proposition('p'),
+                tokens.BinaryOperator(operators.Binary.conjunction),
+                tokens.Proposition('q'),
+                tokens.BracketClose()
+            ]
+            self.assertEqual(computed, expected)
