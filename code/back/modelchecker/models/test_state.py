d0beecbaa56ea26e4efa9f576320ea0713c3b8b0
Laura Baakman <laurabaakman@gmail.com>
2015-03-27 15:25:43 +0100

    Method that gets the outgoing states given an agent.

--- code/back/modelchecker/models/test_state.py
+++ code/back/modelchecker/models/test_state.py
@@ -5,6 +5,7 @@ __author__ = 'laura'
 
 import modelchecker.models as models
 import modelchecker.utils.translators as translators
+import modelchecker.errors as errors
 
 class TestState(TestCase):
     def setUp(self):
@@ -29,3 +30,24 @@ class TestState(TestCase):
         computed = [(source.name, destination.name) for (source, destination) in state.get_all_outgoing_as_two_tuple()]
         expected = {('sa', 'sa'), ('sa','sb'), ('sa', 'sf')}
         self.assertItemsEqual(computed, expected)
+
+    def test_get_all_outgoing_as_two_tuple__with_agent_1(self):
+        state = self.model.get_state_by_name('sa')
+        self.assertRaises(errors.ModelError, state.get_all_outgoing_as_two_tuple, 'nonExistentAgent')
+
+    def test_get_all_outgoing_as_two_tuple__with_agent_2(self):
+        state = self.model.get_state_by_name('sc')
+        agent = '2'
+        computed = [(source.name, destination.name) for (source, destination) in state.get_all_outgoing_as_two_tuple(agent)]
+        expected = {('sc', 'sb'), ('sc', 'sc')}
+        self.assertItemsEqual(computed, expected)
+
+    def test_get_all_outgoing_as_two_tuple__with_agent_3(self):
+        state = self.model.get_state_by_name('sd')
+        agent = '2'
+        computed = [(source.name, destination.name) for (source, destination) in
+                    state.get_all_outgoing_as_two_tuple(agent)]
+        expected = set([])
+        self.assertItemsEqual(computed, expected)
+
+
