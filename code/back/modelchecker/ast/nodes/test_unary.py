43b2484af754a877a6112a9913f55c3763211151
Laura Baakman <laurabaakman@gmail.com>
2015-03-27 15:58:13 +0100

    Tests for the I operator.

--- code/back/modelchecker/ast/nodes/test_unary.py
+++ code/back/modelchecker/ast/nodes/test_unary.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
 
-from modelchecker.ast.nodes import Proposition, Negation, Conjunction, Everybody, Disjunction, Common
+from modelchecker.ast.nodes import Proposition, Negation, Conjunction, Everybody, Disjunction, Common, Implicit
 from modelchecker.models import KModel
 import modelchecker.utils.translators as utils
 
 __author__ = 'laura'
 
 
-class TestUnary(TestCase):
+class TestNegation(TestCase):
     def setUp(self):
         self.model = KModel()
         json_data = utils.read_json('modelchecker/models/test_model_km.json')
@@ -111,4 +111,49 @@ class TestCommon(TestCase):
         self.assertFalse(truth_value)
         # print dict['condition']
         # print dict['conclusion']
+        # print dict['interlude']
+
+class TestImplicit(TestCase):
+    def setUp(self):
+        self.model = KModel()
+        json_data = utils.read_json('modelchecker/models/test_model_km_2.json')
+        self.model = KModel.from_json(json_data)
+        self.lhs = Proposition('p')
+
+    def test_no_relations(self):
+        node = Implicit(self.lhs)
+        (truth_value, dict) = node.is_true(self.model.get_state_by_name('sb'))
+        self.assertTrue(truth_value)
+        print dict['condition']
+        print dict['conclusion']
+
+    def test_one_relation_true(self):
+        node = Implicit(self.lhs)
+        (truth_value, dict) = node.is_true(self.model.get_state_by_name('sa'))
+        self.assertTrue(truth_value)
+        print dict['condition']
+        print dict['conclusion']
+
+    def test_one_relation_false(self):
+        node = Implicit(Proposition('q'))
+        (truth_value, dict) = node.is_true(self.model.get_state_by_name('sa'))
+        self.assertFalse(truth_value)
+        print dict['condition']
+        print dict['conclusion']
+        # print dict['interlude']
+
+    def test_multiple_relations_true(self):
+        node = Implicit(self.lhs)
+        (truth_value, dict) = node.is_true(self.model.get_state_by_name('sd'))
+        self.assertTrue(truth_value)
+        print dict['condition']
+        print dict['conclusion']
+        # print dict['interlude']
+
+    def test_multiple_relations_false(self):
+        node = Implicit(self.lhs)
+        (truth_value, dict) = node.is_true(self.model.get_state_by_name('sd'))
+        self.assertFalse(truth_value)
+        print dict['condition']
+        print dict['conclusion']
         # print dict['interlude']
