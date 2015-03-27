2f35097d9c02b99b1c192502b1e05fb4dacac9cf
Laura Baakman <laurabaakman@gmail.com>
2015-03-27 09:40:42 +0100

    Tested tokenize extensively.

--- code/back/modelchecker/tokenize/test_tokenize.py
+++ code/back/modelchecker/tokenize/test_tokenize.py
@@ -4,6 +4,7 @@ from modelchecker.tokenize.tokenizer import tokenize
 import modelchecker.operators as operators
 from modelchecker.tokenize import tokens
 import modelchecker.config as config
+from modelchecker.errors import  TokenizeError
 
 __author__ = 'laura'
 
@@ -73,5 +74,137 @@ class TestTokenizeProposition(TestCase):
                 tokens.BinaryOperator(operators.Binary.conjunction),
                 tokens.Proposition('q'),
                 tokens.BracketClose()
+            ]
+            self.assertEqual(computed, expected)
+
+class TestTokenizeK(TestCase):
+    def setUp(self):
+        self.logics = ['KM', 'T', 'S4', 'S5']
+
+    def test_tokenize_K(self):
+        expression = 'K_1 p'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.AgentOperator('1', operators.Agent.knowledge),
+                tokens.Proposition('p'),
+            ]
+            self.assertEqual(computed, expected)
+
+    def test_tokenize_M(self):
+        expression = 'M_1 p'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.AgentOperator('1', operators.Agent.possible),
+                tokens.Proposition('p'),
+            ]
+            self.assertEqual(computed, expected)
+
+    def test_tokenize_C(self):
+        expression = 'C p'
+        for logic in self.logics:
+            self.assertRaises(TokenizeError, tokenize, logic, expression)
+
+    def test_tokenize_E(self):
+        expression = 'E p'
+        for logic in self.logics:
+            self.assertRaises(TokenizeError, tokenize, logic, expression)
+
+    def test_tokenize_I(self):
+        expression = 'I p'
+        for logic in self.logics:
+            self.assertRaises(TokenizeError, tokenize, logic, expression)
+
+class TestTokenizeEC(TestCase):
+    def setUp(self):
+        self.logics = ['KEC', 'TEC', 'S4EC', 'S5EC']
+
+    def test_tokenize_K(self):
+        expression = 'K_1 p'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.AgentOperator('1', operators.Agent.knowledge),
+                tokens.Proposition('p'),
+            ]
+            self.assertEqual(computed, expected)
+
+    def test_tokenize_M(self):
+        expression = 'M_1 p'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.AgentOperator('1', operators.Agent.possible),
+                tokens.Proposition('p'),
+            ]
+            self.assertEqual(computed, expected)
+
+    def test_tokenize_C(self):
+        expression = 'C p'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.UnaryOperator(operators.Unary.common),
+                tokens.Proposition('p'),
+            ]
+            self.assertEqual(computed, expected)
+
+    def test_tokenize_E(self):
+        expression = 'E p'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.UnaryOperator(operators.Unary.everybody),
+                tokens.Proposition('p'),
+            ]
+            self.assertEqual(computed, expected)
+
+    def test_tokenize_I(self):
+        expression = 'I p'
+        for logic in self.logics:
+            self.assertRaises(TokenizeError, tokenize, logic, expression)
+
+class TestTokenizeI(TestCase):
+    def setUp(self):
+        self.logics = ['KI', 'TI', 'S4I', 'S5I']
+
+    def test_tokenize_K(self):
+        expression = 'K_1 p'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.AgentOperator('1', operators.Agent.knowledge),
+                tokens.Proposition('p'),
+            ]
+            self.assertEqual(computed, expected)
+
+    def test_tokenize_M(self):
+        expression = 'M_1 p'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.AgentOperator('1', operators.Agent.possible),
+                tokens.Proposition('p'),
+            ]
+            self.assertEqual(computed, expected)
+
+    def test_tokenize_C(self):
+        expression = 'C p'
+        for logic in self.logics:
+            self.assertRaises(TokenizeError, tokenize, logic, expression)
+
+    def test_tokenize_E(self):
+        expression = 'E p'
+        for logic in self.logics:
+            self.assertRaises(TokenizeError, tokenize, logic, expression)
+
+    def test_tokenize_I(self):
+        expression = 'I p'
+        for logic in self.logics:
+            computed = tokenize(logic, expression)
+            expected = [
+                tokens.UnaryOperator(operators.Unary.intention),
+                tokens.Proposition('p'),
             ]
             self.assertEqual(computed, expected)
