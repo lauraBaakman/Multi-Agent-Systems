76d50bc3c5758705a38adb8ce9c78524603590fe
Laura Baakman <laurabaakman@gmail.com>
2015-03-28 13:56:13 +0100

    Passed all parsing gone wrong tests.

--- code/back/modelchecker/ast/test_parser.py
+++ code/back/modelchecker/ast/test_parser.py
@@ -89,6 +89,15 @@ class TestParser(TestCase):
         )
         self.assertEqual(computed_tree, expected_tree)
 
+
+    def test_parse_knowledge_with_brackets(self):
+        computed_tree = Ast.from_string("(K_1 p)", "K").root
+        expected_tree = nodes.Knowledge(
+            "1",
+            nodes.Proposition("p")
+        )
+        self.assertEqual(computed_tree, expected_tree)
+
     def test_parse_double_knowledge(self):
         computed_tree = Ast.from_string("K_1 K_1 p", "K").root
         expected_tree = nodes.Knowledge(
@@ -130,40 +139,37 @@ class TestParser(TestCase):
         with self.assertRaises(errors.ParserError):
             Ast.from_string(formula, "K")
 
-    @skip("Skip test")
     def test_parse_error_4(self):
         formula = "( K_1"
         with self.assertRaises(errors.ParserError):
             Ast.from_string(formula, "K")
 
-    @skip("Skip test")
     def test_parse_error_5(self):
         formula = "K_1 )"
         with self.assertRaises(errors.ParserError):
             Ast.from_string(formula, "K")
 
-    @skip("Skip test")
     def test_parse_error_6(self):
         formula = "& a"
         with self.assertRaises(errors.ParserError):
             Ast.from_string(formula, "K")
 
-    @skip("Skip test")
     def test_parse_error_7(self):
         formula = "a ~ b"
         with self.assertRaises(errors.ParserError):
             Ast.from_string(formula, "K")
 
-    @skip("Skip test")
     def test_parse_error_8(self):
         formula = "a K_1 b"
-        Ast.from_string(formula, "K")
         with self.assertRaises(errors.ParserError):
-            pass
+            Ast.from_string(formula, "K")
 
-    @skip("Skip test")
     def test_parse_error_9(self):
         formula = "a && b"
-        Ast.from_string(formula, "K")
         with self.assertRaises(errors.ParserError):
-            pass
+            Ast.from_string(formula, "K")
+
+    def test_parse_error_10(self):
+        formula = "a b"
+        with self.assertRaises(errors.ParserError):
+            Ast.from_string(formula, "K")
