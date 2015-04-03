cf0bebf492fc80025f9d0a38ee6d79d22d1ffe36
Rick van Veen <r.van.veen133@gmail.com>
2015-04-03 18:05:02 +0200

    dingen

--- code/front/js/gui/gui_info_panel.js
+++ code/front/js/gui/gui_info_panel.js
@@ -7,43 +7,43 @@ define("gui_info_panel", ["d3", "mathjax"], function(d3, JSONEditor, MathJax) {
         var loading = null;
 
         this.send = function() {
-            // d3.json("http://localhost:8000/valuate")
-            //     .on("beforesend", function() {
-            //         loading = document.createElement('div')
-            //         loading.className = 'loading';
-            //         document.getElementById("playground").appendChild(loading);
-            //     })
-            //     .on("load", function(data) {
-            //         var new_child = document.createElement('div');
-            //         new_child.innerHTML = data.motivation;
+            d3.json("http://localhost:8000/valuate")
+                .on("beforesend", function() {
+                    loading = document.createElement('div')
+                    loading.className = 'loading';
+                    document.getElementById("playground").appendChild(loading);
+                })
+                .on("load", function(data) {
+                    var new_child = document.createElement('div');
+                    new_child.innerHTML = data.motivation;
 
-            //         var response = document.getElementById("response");
-            //         while (response.firstChild) {
-            //             response.removeChild(response.firstChild);
-            //         }
-            //         response.appendChild(new_child);
+                    var response = document.getElementById("response");
+                    while (response.firstChild) {
+                        response.removeChild(response.firstChild);
+                    }
+                    response.appendChild(new_child);
 
-            //         MathJax.Hub.Queue(["Typeset", MathJax.Hub, "response"]);
+                    MathJax.Hub.Queue(["Typeset", MathJax.Hub, "response"]);
 
-            //         var new_model = JSON.parse(data.model);
-            //         console.log(new_model);
+                    var new_model = JSON.parse(data.model);
+                    console.log(new_model);
 
-            //         app.get_model().load_from_model_object(new_model);
-            //         app.redraw();
+                    app.get_model().load_from_model_object(new_model);
+                    app.redraw();
 
-            //         document.getElementById("playground").removeChild(loading);
-            //     })
-            //     .on("error", function(error) {
-            //         console.log("error");
-            //         document.getElementById("playground").removeChild(loading);
-            //         console.log(error.title);
+                    document.getElementById("playground").removeChild(loading);
+                })
+                .on("error", function(error) {
+                    console.log("error");
+                    document.getElementById("playground").removeChild(loading);
+                    console.log(error.title);
 
-            //     })
-            //     .post(JSON.stringify({
-            //         state: "0",
-            //         formula: "K_1 M_2 p <-> q",
-            //         model: model.save_to_model_object()
-            //     }));
+                })
+                .post(JSON.stringify({
+                    state: "0",
+                    formula: "K_1 M_2 p <-> q",
+                    model: model.save_to_model_object()
+                }));
 
                 console.log(JSON.stringify({
                     state: "0",
